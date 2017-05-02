7fd2263a9ca52e150181f46a464e2d04c6faef5c
hemmikalligunn <hemmikalligunn@gmail.com>
2017-05-02 12:22:35 +0000

    Setup
    Búa til fallegan menu

--- GSÖ-VSH/innskraning.php
+++ GSÖ-VSH/innskraning.php
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title></title>
+	<link href="styles/style.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 </head>
 <body>
+
+<div class="">
+<?php require './includes/menu.php'; ?>
+</div>
 
 </body>
 </html>
